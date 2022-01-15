1ba5324f2ad21e0319107e4ea6e4bd078b313499
ekeopre beredugo <ekeopreberedugo1@gmail.com>
2022-01-15 17:42:05 +0100

    working on responsiveness for all screens

---
+++ js/menu.js
@@ -0,0 +1,23 @@
+
+const navMenu = document.getElementById("mobile-nav");
+const close =  document.getElementById("close-menu");
+const open =  document.getElementById("open-menu");
+
+
+// open.addEventListener("click", function(){
+
+//     nav.style.display =  "block";
+    
+// })
+
+
+function openMenu(){
+    navMenu.style.display =  "block";
+ 
+}
+
+
+function closeMenu(){
+    navMenu.style.display =  "none"
+}
+
---
+++ js/scrollReveal.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.tagline', { delay: 500 });
+ScrollReveal().reveal('.punchline1', { delay: 2000 });
+ScrollReveal().reveal('.punchline2', { delay: 2500 });
+ScrollReveal().reveal('.punchline3', { delay: 3000 });
